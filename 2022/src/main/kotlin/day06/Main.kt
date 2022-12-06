37648c52def41ee448378d0492ed3a760a463ad7
Philipp Bzdok <philipp.bzdok@mailbox.org>
2022-12-06 20:19:40 +0100

    ext function

--- 2022/src/main/kotlin/day06/Main.kt
+++ 2022/src/main/kotlin/day06/Main.kt
@@ -1,7 +1,7 @@
 package day06
 
 import getResourceAsFile
-import java.io.InputStreamReader
+import java.io.Reader
 
 const val fileName = "day06_input.txt"
 
@@ -11,25 +11,25 @@ fun main() {
     val file = getResourceAsFile(fileName)!!
 
     println("🎄Part 1:")
-    val signalIndex = file.reader().use { findSignalIndex(4, it) }
+    val signalIndex = file.reader().use { it.findSignalIndex(4) }
     println("📞 The signal marker ends with index $signalIndex")
 
     println("🎄Part 2:")
-    val messageIndex = file.reader().use { findSignalIndex(14, it) }
+    val messageIndex = file.reader().use { it.findSignalIndex(14) }
     println("✉️ The message marker ends with index $messageIndex")
 }
 
-fun findSignalIndex(signalSize: Int, reader: InputStreamReader): Int? {
+private fun Reader.findSignalIndex(signalSize: Int): Int? {
     val buffer = Array(signalSize) { ' ' }
     var index = 0
-    var i = reader.read()
+    var i = read()
 
     while (i != -1) {
         buffer[index % signalSize] = i.toChar()
         if (index > (signalSize - 1) && buffer.distinct().count() == signalSize) {
             return index + 1
         }
-        i = reader.read()
+        i = read()
         index++
     }
     return null
